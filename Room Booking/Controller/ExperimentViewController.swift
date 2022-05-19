b381b31ba022123cd7b197a4e6a764d497136bd7
Zac <zacharymeissner1@gmail.com>
2022-05-19 17:33:24 +1000

    testing text label

---
+++ Room Booking/Controller/ExperimentViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ExperimentViewController.swift
+//  Room Booking
+//
+//  Created by Zachary Meissner on 19/5/2022.
+//
+import UIKit
+
+class ExperimentViewController: UIViewController {
+
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ Room Booking/Controller/SearchBookingViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SearchBookingViewController.swift
+//  Room Booking
+//
+//  Created by Zachary Meissner on 19/5/2022.
+//
+import UIKit
+
+class SearchBookingViewController: UIViewController {
+
+    @IBOutlet weak var dateTextField: RoundTextField!
+    @IBOutlet weak var fromTextField: UITextField!
+    
+    @IBOutlet weak var bookingStack: UIStackView!
+    @IBOutlet weak var searchButton: RoundButton!
+    @IBOutlet weak var toTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        searchButton.setColors(bgColor: UIColor.init(named: "SharedBlue")!, tintColor: .white, titleColor: .white)
+        searchButton.setTitle("Search", for: .normal)
+        dateTextField.setSFImage(SFSymbolSystemName: "calender", pointSize: 20, placement: .trailing, imagePadding: 5)
+        for i in 0...5 {
+        let label = UILabel(frame: CGRect(x: 0, y: i * 30, width: 200, height: 21))
+        //label.center = CGPoint(x: 160, y: 285)
+       // label.textAlignment = .center
+        label.text = "I'm a test label"
+
+        self.bookingStack.addSubview(label)
+        }
+    }
+
+}
+
