6278b4cdcb16298fd6d04178d4b3ff5b0a294d00
Zac <zacharymeissner1@gmail.com>
2022-05-20 17:22:29 +1000

    done something in book by date

--- Room Booking/Controller/HomeViewController.swift
+++ Room Booking/Controller/HomeViewController.swift
@@ -1,259 +1,217 @@
 //
-//  HomeViewController.swift
+//  SearchBookingViewController.swift
 //  Room Booking
 //
-//  Created by Zachary Meissner on 20/5/2022.
+//  Created by Zachary Meissner on 19/5/2022.
 //
-
 import UIKit
 
 class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    @IBOutlet weak var dateTextField: RoundTextField!
+    @IBOutlet weak var fromTextField: UITextField!
+    @IBOutlet weak var bookingStack: UIStackView!
+    @IBOutlet weak var searchButton: RoundButton!
+    @IBOutlet weak var toTextField: UITextField!
     
     var collectionview: UICollectionView!
     var cellId = "Cell"
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        searchButton.setColors(bgColor: UIColor.init(named: "SharedBlue")!, tintColor: .white, titleColor: .white)
+        searchButton.setTitle("Search", for: .normal)
+        dateTextField.setSFImage(SFSymbolSystemName: "calender", pointSize: 20, placement: .trailing, imagePadding: 5)
+        //for i in 0...5 {
+        //let label = UILabel(frame: CGRect(x: 0, y: i * 30, width: 200, height: 21))
+        //label.center = CGPoint(x: 160, y: 285)
+       // label.textAlignment = .center
+       // label.text = "I'm a test label"
+                // Create an instance of UICollectionViewFlowLayout since you cant
+                // Initialize UICollectionView without a layout
+                let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+                layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+                layout.itemSize = CGSize(width: view.frame.width, height: 147)
 
-        // Create an instance of UICollectionViewFlowLayout since you cant
-        // Initialize UICollectionView without a layout
-        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        layout.itemSize = CGSize(width: view.frame.width, height: 700)
+                collectionview = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+                collectionview.dataSource = self
+                collectionview.delegate = self
+                collectionview.register(FreelancerCell.self, forCellWithReuseIdentifier: cellId)
+                collectionview.showsVerticalScrollIndicator = false
+                collectionview.backgroundColor = UIColor.white
+                self.view.addSubview(collectionview)
 
-        collectionview = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
-        collectionview.dataSource = self
-        collectionview.delegate = self
-        collectionview.register(FreelancerCell.self, forCellWithReuseIdentifier: cellId)
-        collectionview.showsVerticalScrollIndicator = false
-        collectionview.backgroundColor = UIColor.white
-        self.view.addSubview(collectionview)
-
-    }
+            }
+            
+            
+            func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+                let cell = collectionview.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! FreelancerCell
+                return cell
+            }
+    
+    // This returns the amount of views
+            func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+                return 10
+            }
+    
+        func prepare(imageNo: String, buildingNo: Int, floorNo: Int, roomNo: Int, dateNo: Int, startTime: Int, endTime: Int){
+            
+        }
     
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionview.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! FreelancerCell
-        return cell
-    }
+    
+    
+    
+    
+        }
 
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
-    }
-}
+        class FreelancerCell: UICollectionViewCell {
 
+            let buildingImageView: UIImageView = {
+                
+                let imageView = UIImageView()
+                imageView.backgroundColor = UIColor.white
+                imageView.image = UIImage(named: "image 3")
+                imageView.frame = CGRect(x: 0, y: 0, width: 150, height: 150)
+                let screenSize: CGRect = UIScreen.main.bounds
+                return imageView
+            }()
+            
+            let buildingLabel: UILabel = {
+                let label = UILabel()
+                label.textColor = UIColor.white
+                label.font = UIFont.systemFont(ofSize:(18))
+                label.text = "Building/floor/room"
+                label.translatesAutoresizingMaskIntoConstraints = false
+                return label
+            }()
 
+            let dateLabel: UILabel = {
+                let label = UILabel()
+                label.font = UIFont.systemFont(ofSize:(14))
+                label.textColor = UIColor.white
+                label.text = "Date: "
+                label.translatesAutoresizingMaskIntoConstraints = false
+                return label
+            }()
 
+            let timeLabel: UILabel = {
+                let label = UILabel()
+                label.textColor = UIColor.white
+                label.font = UIFont.systemFont(ofSize:(14))
+                label.text = "Starttime - Endtime"
+                label.translatesAutoresizingMaskIntoConstraints = false
+                return label
+            }()
 
+            let topSeparatorView: UIView = {
+                let view = UIView()
+                view.backgroundColor = UIColor.darkGray
+                view.translatesAutoresizingMaskIntoConstraints = false
+                return view
+            }()
 
+            let bottomSeparatorView: UIView = {
+                let view = UIView()
+                view.backgroundColor = UIColor.darkGray
+                view.translatesAutoresizingMaskIntoConstraints = false
+                return view
+            }()
 
-
-
-class FreelancerCell: UICollectionViewCell {
-
-
-    let profileImageButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = UIColor.white
-        button.layer.cornerRadius = 18
-        button.clipsToBounds = true
-        button.setImage(UIImage(named: "image 3"), for: .normal)
-
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
-
-    let nameLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.systemFont(ofSize:(14))
-        label.textColor = UIColor.darkGray
-        label.text = "Bob Lee"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-
-    let distanceLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = UIColor.lightGray
-        label.font = UIFont.systemFont(ofSize:(14))
-        label.text = "30000 miles"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-    let pricePerHourLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = UIColor.darkGray
-        label.font = UIFont.systemFont(ofSize:(14))
-        label.text = "$40/hour"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-
-
-    let ratingLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = UIColor.lightGray
-        label.font = UIFont.systemFont(ofSize:(14))
-        label.text = "4.9+"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-
-    let showCaseImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.backgroundColor = UIColor.white
-        imageView.image = UIImage(named: "image 3")
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-
-
-    let likesLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = UIColor.lightGray
-        label.font = UIFont.systemFont(ofSize:(14))
-        label.text = "424 likes"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-
-    let topSeparatorView: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor.darkGray
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-
-    let bottomSeparatorView: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor.darkGray
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-
-
-    let likeButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Like", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize:(18))
-        button.setTitleColor(UIColor.darkGray, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
-    let hireButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Hire", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize:(18))
-        button.setTitleColor(UIColor.darkGray, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
-
-    let messageButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Message", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize:(18))
-        button.setTitleColor(UIColor.darkGray, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
+            let confirmButton: UIButton = {
+                let button = UIButton()
+                button.setTitle("Confirm Booking", for: .normal)
+                button.titleLabel?.font = UIFont.systemFont(ofSize:(18))
+                button.setTitleColor(UIColor.white, for: .normal)
+                button.translatesAutoresizingMaskIntoConstraints = false
+                return button
+            }()
 
 
 
-    let stackView: UIStackView = {
-        let sv = UIStackView()
-        sv.axis  = NSLayoutConstraint.Axis.horizontal
-        sv.alignment = UIStackView.Alignment.center
-        sv.distribution = UIStackView.Distribution.fillEqually
-        sv.translatesAutoresizingMaskIntoConstraints = false;
-        return sv
-    }()
+            let stackView: UIStackView = {
+                let sv = UIStackView()
+                sv.axis  = NSLayoutConstraint.Axis.horizontal
+                sv.alignment = UIStackView.Alignment.center
+                sv.distribution = UIStackView.Distribution.fillEqually
+                sv.translatesAutoresizingMaskIntoConstraints = false;
+                return sv
+            }()
 
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+            override init(frame: CGRect) {
+                super.init(frame: frame)
 
-        addViews()
-    }
+                addViews()
+            }
 
-    func addViews(){
-        backgroundColor = UIColor.black
+            func addViews(){
+                backgroundColor = UIColor.black
 
-        addSubview(profileImageButton)
-        addSubview(nameLabel)
-        addSubview(distanceLabel)
-        addSubview(pricePerHourLabel)
-        addSubview(ratingLabel)
-        addSubview(showCaseImageView)
-        addSubview(likesLabel)
+              //  addSubview(profileImageButton)
+                addSubview(dateLabel)
+             //   addSubview(distanceLabel)
+                addSubview(buildingLabel)
+             //   addSubview(ratingLabel)
+                addSubview(buildingImageView)
+                addSubview(timeLabel)
 
-        addSubview(topSeparatorView)
-        addSubview(bottomSeparatorView)
+              //  addSubview(topSeparatorView)
+              //  addSubview(bottomSeparatorView)
 
-        // Stack View
-        addSubview(likeButton)
-        addSubview(messageButton)
-        addSubview(hireButton)
-        addSubview(stackView)
+                // Stack View
+                addSubview(confirmButton)
+        
+                //addSubview(stackView)
 
+                //Date:
+                dateLabel.topAnchor.constraint(equalTo: buildingLabel.bottomAnchor, constant: 15).isActive = true
+                dateLabel.leftAnchor.constraint(equalTo: buildingLabel.leftAnchor).isActive = true
+                
+                //Building/floor/room
+                buildingLabel.topAnchor.constraint(equalTo: topAnchor, constant: 15).isActive = true
+                buildingLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: 10).isActive = true
+                //pricePerHourLabel.centerYAnchor.constraint(equalTo: showCaseImageView.centerYAnchor).isActive = true
 
-        profileImageButton.leftAnchor.constraint(equalTo: leftAnchor, constant: 5).isActive = true
-        profileImageButton.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
-        profileImageButton.heightAnchor.constraint(equalToConstant: 36).isActive = true
-        profileImageButton.widthAnchor.constraint(equalToConstant: 36).isActive = true
+                buildingImageView.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
+                buildingImageView.rightAnchor.constraint(equalTo: buildingLabel.leftAnchor, constant: -20).isActive = true
+                buildingImageView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+                buildingImageView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - 20).isActive = true
 
-        nameLabel.leftAnchor.constraint(equalTo: profileImageButton.rightAnchor, constant: 5).isActive = true
-        nameLabel.centerYAnchor.constraint(equalTo: profileImageButton.centerYAnchor, constant: -8).isActive = true
-        nameLabel.rightAnchor.constraint(equalTo: pricePerHourLabel.leftAnchor).isActive = true
+                confirmButton.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 15).isActive = true
+                confirmButton.leftAnchor.constraint(equalTo: buildingLabel.leftAnchor).isActive = true
+                
+                //starttime - endtime
+                timeLabel.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 15).isActive = true
+                timeLabel.leftAnchor.constraint(equalTo: buildingLabel.leftAnchor).isActive = true
 
-        distanceLabel.leftAnchor.constraint(equalTo: nameLabel.leftAnchor).isActive = true
-        distanceLabel.centerYAnchor.constraint(equalTo: profileImageButton.centerYAnchor, constant: 8).isActive = true
-        distanceLabel.widthAnchor.constraint(equalToConstant: 300).isActive = true
+                //topSeparatorView.topAnchor.constraint(equalTo: likesLabel.bottomAnchor, constant: 10).isActive = true
+               // topSeparatorView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+               // topSeparatorView.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
 
-        pricePerHourLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -10).isActive = true
-        pricePerHourLabel.centerYAnchor.constraint(equalTo: nameLabel.centerYAnchor).isActive = true
+                //stackView.addArrangedSubview(likeButton)
+               // stackView.addArrangedSubview(hireButton)
+               // stackView.addArrangedSubview(messageButton)
 
-        // Distance depeneded on the priceLabel and distance Label
-        ratingLabel.rightAnchor.constraint(equalTo: pricePerHourLabel.rightAnchor).isActive = true
-        ratingLabel.centerYAnchor.constraint(equalTo: distanceLabel.centerYAnchor).isActive = true
+                //stackView.topAnchor.constraint(equalTo: topSeparatorView.bottomAnchor, constant: 4).isActive = true
+               // stackView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+               // stackView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
 
-        showCaseImageView.topAnchor.constraint(equalTo: profileImageButton.bottomAnchor, constant: 10).isActive = true
-        showCaseImageView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
-        showCaseImageView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - 20).isActive = true
-
-        likesLabel.topAnchor.constraint(equalTo: showCaseImageView.bottomAnchor, constant: 10).isActive = true
-        likesLabel.leftAnchor.constraint(equalTo: profileImageButton.leftAnchor).isActive = true
-
-        topSeparatorView.topAnchor.constraint(equalTo: likesLabel.bottomAnchor, constant: 10).isActive = true
-        topSeparatorView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
-        topSeparatorView.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
-
-        stackView.addArrangedSubview(likeButton)
-        stackView.addArrangedSubview(hireButton)
-        stackView.addArrangedSubview(messageButton)
-
-        stackView.topAnchor.constraint(equalTo: topSeparatorView.bottomAnchor, constant: 4).isActive = true
-        stackView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
-        stackView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-
-        bottomSeparatorView.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 4).isActive = true
-        bottomSeparatorView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
-        bottomSeparatorView.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
+               // bottomSeparatorView.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 4).isActive = true
+               // bottomSeparatorView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+               // bottomSeparatorView.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
 
 
-    }
+            }
 
 
 
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+            required init?(coder aDecoder: NSCoder) {
+                fatalError("init(coder:) has not been implemented")
+            }
 
-}
+        }
+
+
+
+
